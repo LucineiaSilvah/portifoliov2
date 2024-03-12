37b3edd387980c0e9c01909296e8af18142a8426
luh <lucineia.rsilvah@gmail.com>
2024-03-12 19:47:54 -0300

    refatorando codigo

--- src/app.js
+++ src/app.js
@@ -1,8 +1,97 @@
 const botao = document.querySelector('.fa-jet-fighter-up');
 const corpo = document.querySelector('html')
-
+const projetosBase = '/src/db.json'
 document.addEventListener('scroll', vereficar)
+const plus = document.getElementById('plus')
+const lista = document.getElementById('lista')
+const mode = document.getElementById('theme')
 
+theme.addEventListener('click',()=>{
+  document.body.classList.toggle('dark')
+})
+
+fetch(projetosBase).then(res=> res.json()).then(projetos =>{
+ 
+  console.log(projetos[1].plus);
+  projetos[0].destaque.forEach(projeto => {
+   lista.innerHTML += `
+
+ <li class="experiencias__card">
+ <img
+   class="experiencias__imagem"
+   src="${projeto.imagem}"
+   alt=""
+ />
+ <div class="experiencias__info">
+   <p class="titulo">${projeto.titulo}</p>
+   <p class="descricao">${projeto.descricao}</p>
+   <p class="tech">${projeto.tech}</p>
+   <div class="experiencias__botaoes">
+     <button class="experiencias__botao">
+       <a class="repo"
+         href="${projeto.repo}"
+         target="_blank"
+         rel="noopener noreferrer"
+         >Repositorio</a
+       >
+     </button>
+     <button class="experiencias__botao experiencias__botao--demo">
+       <a
+         class="demo"
+         href="${projeto.demo}"
+         target="_blank"
+         rel="noopener noreferrer"
+         >Ver demo</a
+       >
+     </button>
+   </div>
+ </div>
+</li>
+
+  
+   `
+  });
+ 
+})
+
+fetch(projetosBase).then(res=> res.json()).then(projetos =>{
+ projetos[1].plus.forEach(projeto =>{
+  plus.innerHTML += `
+
+  <li class="experiencias__card">
+  <img
+    class="experiencias__imagem"
+    src="${projeto.imagem}"
+    alt=""
+  />
+  <div class="experiencias__info">
+    <p class="titulo">${projeto.titulo}</p>
+    <p class="descricao">${projeto.descricao}</p>
+    <p class="tech">${projeto.tech}</p>
+    <div class="experiencias__botaoes">
+      <button class="experiencias__botao">
+        <a class="repo"
+          href="${projeto.repo}"
+          target="_blank"
+          rel="noopener noreferrer"
+          >Repositorio</a
+        >
+      </button>
+      <button class="experiencias__botao experiencias__botao--demo">
+        <a
+          class="demo"
+          href="${projeto.demo}"
+          target="_blank"
+          rel="noopener noreferrer"
+          >Ver demo</a
+        >
+      </button>
+    </div>
+  </div>
+ </li>
+ `
+ })
+})
 function vereficar(){
   if(scrollY > 350){
     botao.style.visibility = 'visible'
@@ -26,13 +115,5 @@ menu.addEventListener('click', ()=>{
   
 })
 
-let modo = document.getElementById('mode')
- 
-   modo.addEventListener('click', ()=> {
-   document.querySelector('.container').classList.toggle('white')
-
-  
 
 
-
- })
